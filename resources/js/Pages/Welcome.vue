16df5d83bc9cb68b8e61ca42bed4e65c8d18c15e
Drizo7 <adriankadyalunda@gmail.com>
2024-04-03 09:52:16 +0200

    added icons

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -34,10 +34,13 @@ function handleImageError() {
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white"
         >
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
+                <h1 class="flex justify-center text-4xl font-extrabold text-black mb-6">Welcome to Leave Portal</h1>
+                 <p class="flex justify-center text-lg text-gray-800 mb-4">Manage your leave requests with ease.</p>
+                 
                 <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
+                    
                     <div class="flex lg:justify-center lg:col-start-2">
                         
-                    
                     <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-center">
                         
                         <Link
@@ -49,9 +52,10 @@ function handleImageError() {
                         </Link>
 
                         <template v-else>
+                            <div class="flex justify-center space-x-4 mb-8">
                             <Link
                                 :href="route('login')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                class="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
                                 Log in
                             </Link>
@@ -59,10 +63,11 @@ function handleImageError() {
                             <Link
                                 v-if="canRegister"
                                 :href="route('register')"
-                                class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                                class="inline-flex items-center justify-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-gray-700 bg-gray-300 hover:bg-gray-400 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
                                 Register
                             </Link>
+                        </div>
                         </template>
                     </nav>
                 </div>
